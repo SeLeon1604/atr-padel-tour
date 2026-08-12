bd54dbe86ad3bc353d781c64d44fd762436229c2
SeLeon1604 <116900919+SeLeon1604@users.noreply.github.com>
2026-08-12 11:50:33 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ATR Padel Tour — Service Worker v22
-const CACHE_NAME = 'atr-padel-v27';
+const CACHE_NAME = 'atr-padel-v28';
 const BASE = '/atr-padel-tour';
 const ASSETS = [
   BASE + '/index.html',
